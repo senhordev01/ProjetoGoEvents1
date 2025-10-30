2b337ef1b4ae8b67d10a96169e450a7147a5eeff
Rogerio <contaprogramacao230@gmail.com>
2025-10-30 17:27:30 -0300

    Terceiro Commit

--- src/app/tarefas/tarefas.ts
+++ src/app/tarefas/tarefas.ts
@@ -8,12 +8,21 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './tarefas.html',
   styleUrl: './tarefas.css'
 })
-export class Tarefas {
+export class Tarefas implements OnInit {
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     const projetoID = this.route.snapshot.paramMap.get('id');
     console.log('Projeto carregado:', projetoID);
+  }
+
+  CriarTarefas:string[] = [];
+  Contador:number = 1;
+
+  TarefasNovas(event:any){
+    const marcadores = `Tarefa ${this.Contador}`;
+    this.CriarTarefas.push(marcadores);
+    this.Contador++;
   }
   
 }
