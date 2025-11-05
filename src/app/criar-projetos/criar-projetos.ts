20e2917e9e1e2b332dd31a6ae98cc00307ff3d1b
Rogerio <contaprogramacao230@gmail.com>
2025-11-05 01:22:17 -0300

    Nono Commit

--- src/app/criar-projetos/criar-projetos.ts
+++ src/app/criar-projetos/criar-projetos.ts
@@ -14,6 +14,7 @@ export class CriarProjetos {
   nomeRepresentante:string = "";
   emailRepresentante:string = "";
   previsaoTermino:string = "";
+  minPrevisao:string = "2025-01-01";
   private dialogRef = inject(DialogRef, { optional: true });
 
   FecharJanela() {
@@ -21,9 +22,21 @@ export class CriarProjetos {
   }
 
   criarProjeto() {
+    const Previsao = new Date(this.previsaoTermino);
+    const limiteMinimo = new Date(this.minPrevisao);
     try {
       if (this.nomeProjeto.trim() === "") {
         throw new Error("O projeto nao pode ficar sem um nome");
+      }else if(this.nomeRepresentante.trim() === ""){
+        throw new Error("O nao é possível criar um projeto sem o nome do representante ");
+      }else if(this.emailRepresentante.trim()=== ""){
+        throw new Error("O nao é possível criar um projeto sem o email do representante ");
+      }else if(!this.emailRepresentante.includes("@") && !this.emailRepresentante.includes(".com")){
+        throw new Error("A formataçao do email está incorreta");
+      }else if(this.previsaoTermino === ""){
+        throw new Error("O campo da previsao nao pode ficar vazio");
+      }else if (Previsao < limiteMinimo){
+        throw new Error("Data abaixo do limite");
       } else {
         this.dialogRef?.close(this.nomeProjeto);
       }
